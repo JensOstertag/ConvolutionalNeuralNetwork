55e1d5f1afe9e4ab53ff00014bd0dcbe0de6596c
JensOstertag <JensOstertag@gmx.de>
2022-05-29 11:26:32 +0200

    Implement CNN Propagation

--- src/de/jensostertag/cnn/neuralnetwork/CNN.java
+++ src/de/jensostertag/cnn/neuralnetwork/CNN.java
@@ -1,5 +1,6 @@
 package de.jensostertag.cnn.neuralnetwork;
 
+import de.jensostertag.cnn.neuralnetwork.layers.FullyConnectedLayer;
 import de.jensostertag.cnn.neuralnetwork.layers.Layer;
 
 import java.util.ArrayList;
@@ -9,5 +10,15 @@ public class CNN {
     
     public void addLayer(Layer layer) {
         this.layers.add(layer);
+    }
+    
+    public Object[] propagate(Object input) {
+        Object[] output = new Object[this.layers.size() + 1];
+        output[0] = input;
+    
+        for(int i = 0; i < this.layers.size(); i++)
+            output[i + 1] = this.layers.get(i).propagate(output[i]);
+    
+        return output;
     }
 }
