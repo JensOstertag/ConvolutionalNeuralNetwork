4690ea5088679bafb4ba064c8b4b87818cfbd740
JensOstertag <JensOstertag@gmx.de>
2022-06-15 12:51:04 +0200

    Update Layer Interface

--- src/de/jensostertag/cnn/neuralnetwork/layers/Layer.java
+++ src/de/jensostertag/cnn/neuralnetwork/layers/Layer.java
@@ -2,6 +2,5 @@ package de.jensostertag.cnn.neuralnetwork.layers;
 
 public interface Layer {
     Object propagate(Object input);
-    Object mistakes(Object previousMistakes, Object layerOutput);
-    void optimizeWeights(Object previousMistakes, Object layerOutput, double learningRate);
+    Object backPropagate(Object d_L_d_Y, Object input, double learningRate);
 }
