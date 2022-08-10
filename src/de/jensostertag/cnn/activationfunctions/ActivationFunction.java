20273862437826277f850febcd0581fd1d1ccb67
JensOstertag <JensOstertag@gmx.de>
2022-08-10 13:06:21 +0200

    JavaDoc for Activation Function

--- src/de/jensostertag/cnn/activationfunctions/ActivationFunction.java
+++ src/de/jensostertag/cnn/activationfunctions/ActivationFunction.java
@@ -6,7 +6,24 @@ public interface ActivationFunction {
     Sigmoid activationSigmoid = new Sigmoid();
     Step activationStep = new Step();
     
+    /**
+     * Activation Function
+     * @param x Input
+     * @return Activation Function of Input
+     */
     double function(double x);
+    
+    /**
+     * Derivative of Activation Function
+     * @param x Input
+     * @return Derivative of Activation Function of Input
+     */
     double derivative(double x);
+    
+    /**
+     * Inverse of Activation Function
+     * @param y Output
+     * @return Input where Activation Function of Input is Output
+     */
     double where(double y);
 }
