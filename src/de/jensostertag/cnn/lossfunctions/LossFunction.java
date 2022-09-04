b4270e0a95e329b72425f14edcb7a344040d489b
JensOstertag <JensOstertag@gmx.de>
2022-09-04 11:57:25 +0200

    JavaDoc for Loss Functions

--- src/de/jensostertag/cnn/lossfunctions/LossFunction.java
+++ src/de/jensostertag/cnn/lossfunctions/LossFunction.java
@@ -5,6 +5,19 @@ public interface LossFunction {
     MeanSquaredError lossMeanSquaredError = new MeanSquaredError();
     SquaredError lossSquaredError = new SquaredError();
     
+    /**
+     * Loss Function
+     * @param expected Expected Value
+     * @param actual Actual Value
+     * @return Loss between actual and expected Value
+     */
     double function(double[] expected, double[] actual);
+    
+    /**
+     * Derivative of Loss Function
+     * @param expected Expected Value
+     * @param actual Actual Value
+     * @return Derivative of Loss between actual and expected Value
+     */
     double[] derivative(double[] expected, double[] actual);
 }
